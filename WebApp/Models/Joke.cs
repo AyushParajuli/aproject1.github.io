aefa2a0b013b3a10b489ba09f095ee553cc3eab2
KawakiParajuli <king.gabu1234@gmail.com>
2024-01-14 18:27:18 -0500

    pppp

---
+++ WebApp/Models/Joke.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApp.Models
+{
+    public class Joke
+    {
+        //for all the public property i typed prop and double tab.
+        public int Id { get; set; } // it will have a by default public, int -> datatype,
+                                      // my property -> data type which then i changed to "Id".
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        // for all the method i typed ctor and tab.
+        public Joke()
+        {
+            
+        }
+    }
+}
